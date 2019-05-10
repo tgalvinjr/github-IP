a2ce0465aace99d87562a246c6328ecc8e52860b
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-10 05:19:42 +0300

    add navbar on app component.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user/user.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     UserComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpModule //import http module
   ],
-  providers: [],
+  providers: [], //add service to providers
   bootstrap: [AppComponent]
 })
 export class AppModule { }
