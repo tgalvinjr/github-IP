ce8568ed03a66470e4951b5873b209bb6003b2c6
tgalvinjr <tgalvin.jr@gmail.com>
2019-05-10 05:12:45 +0300

    add logic to user services

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { map } from 'rxjs/operators';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class UserService {
 
-  constructor() { }
+  private username= 'tgalvinjr';
+  languageMap = new Map();
+
+  constructor(private _http: Http) {
+    console.log('Github service init');
+  }
+
+  getUser() {
+    return this._http.get(`https://api.github.com/users/${this.username}`)
+      .pipe(map(res => res.json()));
+  }
+  getRepos() {
+    return this._http.get(`https://api.github.com/users/${this.username}/repos?per_page=100`)
+      .pipe(map(res => res.json()));
+  }
+  updateUsername(username: string) {
+    this.username = username;
+  }
+  getLanguages(url: string) {
+    return this._http.get(url).pipe(map(res => res.json()));
+  }
+  storeLanguageMap(language: Object) {
+    var languageArray = Object.keys(language);
+    for (var j = 0; j < languageArray.length; j++) {
+      var name = languageArray[j];
+      var value = language[name];
+      console.log(value);
+      if (!this.languageMap.get(name)) {
+        this.languageMap.set(name, value);
+      } else {
+        var tempVal = this.languageMap.get(name);
+        tempVal += value;
+        this.languageMap.set(name, tempVal);
+      }
+      
+    }
+  }
+  getLanguageMap() {
+    return this.languageMap;
+  }
+
 }
